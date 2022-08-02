43767b66b0f66e8aa0de6d0de0fdf454d3f1deb2
chingizmalyukov <killerban4@gmail.com>
2022-08-02 21:19:49 +0300

    first-day-work-result

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def summ(n):
+    summ_numbers = 0
+    while n != 0:
+        summ_numbers += n % 10
+        n //= 10
+    return summ_numbers
+def count(n):
+    count_numbers = 0
+    while n != 0:
+        n //= 10
+        count_numbers += 1
+    return count_numbers
+
+n = int(input('Введите N: '))
+print('')
+print('Сумма чисел:', summ(n))
+print('Количество цифр в числе:', count(n))
+print('Разность суммы и количества цифр:', summ(n)-count(n))
+
+#логично было бы сделать все в одной функции, просто добавить счетчик итераций цикла.
+#и его использовать, как кол-во чисел. НО в ТЗ просили другое =)
