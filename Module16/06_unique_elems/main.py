7c0964a2f299e5913d57da2323fa5f6e07c1a811
chingizmalyukov <killerban4@gmail.com>
2022-08-05 16:12:00 +0300

    6 tasks complete

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+first_list = []
+second_list = []
+
+for i in range (3):
+    print('Введите', i+1, '-е число для первого списка:', end=' ')
+    num = int(input())
+    first_list.append(num)
+
+for i in range (7):
+    print('Введите', i+1, '-е число для второго списка:', end=' ')
+    num = int(input())
+    second_list.append(num)
+
+
+print('\nПервый список:', first_list)
+print('Второй список:', second_list)
+
+first_list.extend(second_list)
+sorted_list = list(set(first_list))
+
+print('\nНовый первый список с уникальными элементами:', sorted_list)
