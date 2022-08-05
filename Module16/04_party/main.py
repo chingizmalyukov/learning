cd1a239036163c18017d34f89e34ee75926774e2
chingizmalyukov <killerban4@gmail.com>
2022-08-06 00:32:54 +0300

    rework 3 and tasks

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,7 +1,7 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-
 while True:
+    flag = False
     print('Сейчас на вечеринке', len(guests), 'человек:', guests)
     answer = input('Гость пришел или ушел? ')
     if answer == 'Пора спать':
@@ -9,9 +9,14 @@ while True:
         break
     name = input('Имя гостя: ')
     if answer == 'ушел':
-        # TODO если имя окажется недопустимым, произойдёт ошибка. Нужно добавить проверку на наличие имени в списке
-        guests.remove(name)
-        print('Пока,', name)
+        for i_name in guests:
+            if name == i_name:
+                flag = True
+        if flag:
+            guests.remove(name)
+            print('Пока,', name)
+        else:
+            print('Нет таких!')
     elif answer == 'пришел':
         if len(guests) >= 6:
             print('Прости,', name, ', но мест нет.')
