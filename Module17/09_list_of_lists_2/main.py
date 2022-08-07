52b965d993365249daa8725eff53d838479bf1e9
chingizmalyukov <killerban4@gmail.com>
2022-08-07 17:26:37 +0300

    tasks done

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+finish_list = [num for list_inside in nice_list for next_list_inside in list_inside for num in next_list_inside]
+
+print(finish_list)
