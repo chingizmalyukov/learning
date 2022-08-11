6cb325b808fa2eb0706fc8b3513d6435b9c3d797
chingizmalyukov <killerban4@gmail.com>
2022-08-11 17:46:17 +0300

    all tasks done

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def solution(first_string, second_string):
+    flag = False
+    count = 0
+    if first_string == second_string:
+        count = 0
+        flag = True
+    elif len(first_string) == len(second_string):
+        for i in range(len(first_string)):
+            first_string = first_string[-1:] + first_string[:-1]
+            if first_string == second_string:
+                count = i + 1
+                flag = True
+    return flag, count
+
+
+first_string = input('Первая строка: ')
+second_string = input('Вторая строка: ')
+flag, count = solution(first_string, second_string)
+
+if flag:
+    print('\nПервая строка получается из второй со сдвигом', count)
+else:
+    print('\nПервую строку нельзя получить из второй с помощью циклического сдвига.')
--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+string = input('Сообщение: ')
+solved_string = ''
+start = 0
+
+for i in range(len(string)):
+    if string[i].isalpha() == False:
+        new_word = string[start:i]
+        solved_string += new_word[::-1] + string[i]
+        start = i + 1
+    elif string[i].isalpha() == True and i == len(string) - 1:
+        new_word = string[start:i]
+        solved_string += new_word[::-1]
+
+print('Новое сообщение:', solved_string)
