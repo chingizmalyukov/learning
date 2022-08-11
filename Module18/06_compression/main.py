1a90437f30879e1753aba42b7ea033064fdd224b
chingizmalyukov <killerban4@gmail.com>
2022-08-11 15:47:55 +0300

    added solution for 6 and 7 tasks

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+zipped_string = ''
+letter_number = 0
+count = 0
+
+for i, symbol in enumerate(string):
+    if symbol == string[letter_number]:
+        count += 1
+    else:
+        zipped_string = (''.join([zipped_string, string[letter_number], str(count)]))
+        letter_number = i
+        count = 1
+zipped_string = (''.join([zipped_string, string[letter_number], str(count)]))
+
+
+print(zipped_string)
--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def ip_check(string):
+    flag = True
+    if len(string) != 4:
+        print('Адрес — это четыре числа, разделённые точками.')
+    for symbol in string:
+        if not symbol.isdigit():
+            print(symbol, '— это не целое число.')
+            flag = False
+            break
+        elif int(symbol) > 255:
+            print(symbol, 'превышает 255.')
+            flag = False
+            break
+        elif int(symbol) < 0:
+            print(symbol, 'меньше 0')
+            flag = False
+            break
+    return flag
+
+string = input('Введите IP: ').split('.')
+
+if ip_check(string):
+    print('IP-адрес корректен')
