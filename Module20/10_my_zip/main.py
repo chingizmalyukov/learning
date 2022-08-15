90669c23a984bae94af2a21bed565d73f58ddcd7
chingizmalyukov <killerban4@gmail.com>
2022-08-15 19:44:52 +0300

    all tasks done

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def action(Left, Right):
+    for i in range(min(len(Left), len(Right))):
+        yield Left[i], Right[i]
+
+string = 'abcd'
+numbers = (10, 20, 30, 40, 50)
+solution = action(string, numbers)
+print('\nРезультат:\n', solution)
+for letter, dig in solution:
+    print((letter, dig))
