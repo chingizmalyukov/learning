e5b7e0be6b453f04f6948e4cd4682a9860cb8a5a
chingizmalyukov <killerban4@gmail.com>
2022-09-07 18:30:34 +0300

    added 6 task solution

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+
+file = open('text.txt')
+solution = open('cipher_text.txt', 'a')
+
+for i, i_elem in enumerate(file):
+    string = ''
+    for j_elem in i_elem:
+        if j_elem == '\n':
+            break
+        code = ord('a') + (ord(j_elem) - ord('a') + (i + 1) % (ord('z') - ord('a') + 1))
+        string += chr(code)
+    solution.write(string)
+    solution.write('\n')
+
+file.close()
+solution.close()
