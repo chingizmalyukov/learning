04b759d20e52d951631ed93b2afdae016403f790
chingizmalyukov <killerban4@gmail.com>
2022-09-07 15:37:18 +0300

    1 task done

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+solution = 0
+
+file = open('numbers.txt', 'r')
+for i_elem in file:
+    for j_elem in i_elem:
+        if j_elem.isdigit():
+            solution += int(j_elem)
+file.close()
+
+answer = open('answer.txt', 'w')
+answer.write(str(solution))
+answer.close()
