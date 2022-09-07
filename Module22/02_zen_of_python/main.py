ec7bcb9f4f989cdd65d2b581942114f02e38decc
chingizmalyukov <killerban4@gmail.com>
2022-09-07 15:49:43 +0300

    added 2 task solution

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+file = open('zen.txt', 'r')
+text = []
+for i_elem in file:
+    text.append(i_elem)
+file.close()
+
+for string in reversed(text):
+    print(string, end='')
