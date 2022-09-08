65c035db349542d086efcbb199d5a4fa8e5d3d2a
Alexandra <aguseva@mail.com>
2022-09-08 10:47:54 +0200

    решение зачтено

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -6,9 +6,3 @@ file.close()
 
 for string in reversed(text):
     print(string)
-
-# TODO между первой и второй строкой отсутствует перенос:
-# Namespaces are one honking great idea -- let's do more of those!If the implementation is easy to explain, it may be a good idea.
-# If the implementation is hard to explain, it's a bad idea.
-# TODO в код нужно добавить обработку для этого случая,
-#  чтобы каждое предложение отображалось на новой строке
