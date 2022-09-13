43629c2746e82a8d442c5e50f6c841bda6237cbf
chingizmalyukov <killerban4@gmail.com>
2022-09-13 19:46:14 +0300

    5 task solved

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+result = []
+
+with open('calc.txt', 'r') as file:
+    for line in file.readlines():
+        try:
+            if line.endswith('\n'):
+                result.append(eval(line[1:-2]))
+            else:
+                result.append(eval(line[1:]))
+        except:
+            if input('Обнаружена ошибка:' + line[1:] + 'Хотите исправить? ').lower() == 'да':
+                line = input('Введите исправленную строку: ')
+                result.append(eval(line))
+
+print(sum(result))
