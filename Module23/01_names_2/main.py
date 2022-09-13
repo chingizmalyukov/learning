8448448ab73861249c34e06f3ed586fc4bf78be6
chingizmalyukov <killerban4@gmail.com>
2022-09-13 20:23:57 +0300

    all tasks DONE!

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,25 +1,24 @@
 def solution(file_name):
     solution_summ = 0
     string_number = 0
-    with open(file_name, 'r', encoding='utf-8') as file,\
-            open('errors.txt', 'a', encoding='utf-8') as errors_log:
-        try:
-            for i_name in file:
-                string_number += 1
-                if i_name.endswith('\n'):
-                    name = i_name[:-1]
-                else:
-                    name = i_name
+    with open(file_name, 'r', encoding='utf-8') as file, \
+            open('errors.txt', 'w', encoding='utf-8') as errors_log:
+        for i_name in file:
+            string_number += 1
+            if i_name.endswith('\n'):
+                name = i_name[:-1]
+            else:
+                name = i_name
+            try:
                 if len(name) < 3:
-                    raise TypeError
-                else:
-                    solution_summ += len(name)
-
-        except TypeError:
-            print(f'Ошибка! В троке {string_number} менее 3 символов!')
-            errors_log.write(str(f'Ошибка! В троке {string_number} менее 3 символов!'))
-            errors_log.write('\n')
+                    raise ZeroDivisionError
+            except ZeroDivisionError:
+                print(f'Ошибка: менее трёх символов в строке {string_number}')
+                errors_log.write(str(f'Ошибка! В троке {string_number} менее 3 символов!\n'))
+            else:
+                solution_summ += len(name)
     return solution_summ
+
 
 file_name = 'people.txt'
 print(solution(file_name))
--- Module23/06_chat/main.py
+++ Module23/06_chat/main.py
@@ -1,1 +1,42 @@
-# TODO здесь писать код
+def logoutput(file):
+    try:
+        log = open(file, 'r', encoding='utf-8')
+        for line in log:
+            print(line)
+        log.close()
+    except FileNotFoundError:
+        print('Лога чата нет. Введите первое сообщение!\n')
+
+
+def loginput(file, name, message):
+    log = open(file, 'a')
+    logged_message = name + ' ' + message + '\n'
+    log.write(str(logged_message))
+    log.close()
+
+
+chat_log = dict()
+name = input('ВВедите логин: ') + ':'
+
+try:
+    while True:
+        print('Выберите одно из действий: \n 1 - Посмотреть текущий текст чата.'
+              ' \n 2 - Отправить сообщение (затем вводит сообщение).'
+              ' \n любое другое значение - разлогиниться.')
+        answer = input('')
+        if answer == '1':
+            logoutput('log.txt')
+        elif answer == '2':
+            print('Введите сообщение: ')
+            print(name, end=' ')
+            message = input('')
+            loginput('log.txt', name, message)
+        else:
+            raise ZeroDivisionError
+except ZeroDivisionError:
+    print(f'{name}, покинул чат!')
+
+# Почему-то при использовании кириллицы, и выводе лога чата,
+# программа выдает ошибку, что-то там про инкодинг,
+# хотя я указал его в функции вывода лога.
+# С латиницей таких проблем нет.
