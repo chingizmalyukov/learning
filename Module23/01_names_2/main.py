a34643e41775032ec585e056f9252ae5be312856
chingizmalyukov <killerban4@gmail.com>
2022-09-13 18:13:22 +0300

    solved 1 and 2 tasks

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def solution(file_name):
+    solution_summ = 0
+    string_number = 0
+    with open(file_name, 'r', encoding='utf-8') as file,\
+            open('errors.txt', 'a', encoding='utf-8') as errors_log:
+        try:
+            for i_name in file:
+                string_number += 1
+                if i_name.endswith('\n'):
+                    name = i_name[:-1]
+                else:
+                    name = i_name
+                if len(name) < 3:
+                    raise TypeError
+                else:
+                    solution_summ += len(name)
+
+        except TypeError:
+            print(f'Ошибка! В троке {string_number} менее 3 символов!')
+            errors_log.write(str(f'Ошибка! В троке {string_number} менее 3 символов!'))
+            errors_log.write('\n')
+    return solution_summ
+
+file_name = 'people.txt'
+print(solution(file_name))
--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -13,27 +13,25 @@ def f2(x, y):
     return y / x
 
 
-try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
+with open('coordinates.txt', 'r') as input, open('result.txt', 'a') as output:
+    count = 1
+    for line in input:
+        function_num = 1
         nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
         try:
+            res1 = f(int(nums_list[0]), int(nums_list[1]))
+            function_num += 1
             res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+            number = random.randint(0, 100)
+            my_list = sorted([res1, res2, number])
+            output.write(str(my_list))
+            output.write('\n')
+        except ZeroDivisionError as er:
+            print(f"Ошибка в {count} итерации: "
+                  f"\nДелитель в {function_num} функции = 0, а на ноль делить нельзя!")
+            output.write('error\n')
+        count += 1
 
-
-# TODO отредактировать и исправить программу
+# Убрал лишние пары, поправил вывод, чтобы было понятно на какой иенно итерации
+# происходит ошибка. Также в файл теперь записывается слово "error" в строку,
+# в которой произошла данная ошибка.
