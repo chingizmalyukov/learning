a398cb4eb92013c17ca95a5d6ddc83d422192ff7
chingizmalyukov <killerban4@gmail.com>
2022-09-21 21:56:32 +0300

    3 task solved

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default=0):
+        return super().get(key, default)
+
+
+def_dict = {0: 'a', 1: 'b', 3: 'c'}
+
+dict_1 = MyDict(def_dict)
+print(dict_1.get(1))
