b25832cfea65eef7bd3631edc70d500279601b9f
chingizmalyukov <killerban4@gmail.com>
2022-09-21 23:26:42 +0300

    5 tasks solved

--- Module25/04_company/main.py
+++ Module25/04_company/main.py
@@ -1,1 +1,84 @@
-# TODO здесь писать код
+class Person:
+    def __init__(self, name, surmane, age):
+        self.set_name(name)
+        self.set_surname(surmane)
+        self.set_age(age)
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_surname(self, surname):
+        self.__surname = surname
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_surname(self):
+        return self.__surname
+
+
+class Employee(Person):
+    def payment(self):
+        pass
+
+    def __str__(self):
+        return f'имя: {self.get_name()}, фамилия: {self.get_surname()}, з\п: {self.payment()}'
+
+
+class Manager(Employee):
+
+    def payment(self):
+        return 13000
+
+
+class Agent(Employee):
+    __salles = 0
+
+    def payment(self):
+        return 5000 + self.get_salles() * 5 / 100
+
+    def set_salles(self, salles):
+        self.__salles = salles
+
+    def get_salles(self):
+        return self.__salles
+
+
+class Worker(Employee):
+    __hours = 0
+
+    def payment(self):
+        return 100 * self.get_hours()
+
+    def set_hours(self, hours):
+        self.__hours = hours
+
+    def get_hours(self):
+        return self.__hours
+
+
+sallary_summ = 0
+
+manager_1 = Manager('Tom', 'Mot', 20)
+manager_2 = Manager('Kom', 'Kot', 30)
+manager_3 = Manager('Fom', 'Fot', 40)
+agent_1 = Agent('Tom', 'Mot', 20)
+agent_2 = Agent('Kom', 'Kot', 30)
+agent_3 = Agent('Fom', 'Fot', 40)
+agent_1.set_salles(100)
+agent_2.set_salles(100)
+agent_3.set_salles(100)
+worker_1 = Worker('Tom', 'Mot', 20)
+worker_2 = Worker('Kom', 'Kot', 30)
+worker_3 = Worker('Fom', 'Fot', 40)
+worker_1.set_hours(10)
+worker_2.set_hours(10)
+worker_3.set_hours(10)
+
+workers_list = [manager_1, manager_2, manager_3, agent_1, agent_2, agent_3, worker_1, worker_2, worker_3]
+
+for human in workers_list:
+    print(human)
--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,1 +1,77 @@
-# TODO здесь писать код
+import math
+
+
+class Car:
+    __angel = 90
+    __x = 0
+    __y = 0
+
+    def move(self, dist):
+        x = self.get_x() + dist * math.cos(self.get_angel())
+        self.set_x(x)
+        y = self.get_y() + dist * math.sin(self.get_angel())
+        self.set_y(y)
+
+    def change(self, turn):
+        if turn == 0:
+            if self.get_angel() == 360:
+                self.set_angel(0)
+                self.change_angel(+90)
+        elif turn == 1:
+            if self.get_angel() == 0:
+                self.set_angel(360)
+                self.change_angel(-90)
+
+    def set_x(self, x):
+        self.__x = x
+
+    def set_y(self, y):
+        self.__y = y
+
+    def set_angel(self, angel):
+        self.__angel = angel
+
+    def change_angel(self, turn):
+        self.__angel += turn
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def get_angel(self):
+        return self.__angel
+
+
+class Bus(Car):
+    __passengers = 0
+    __max_passengers = 90
+    __ticket = 1
+    __money = 0
+
+    def get_passengers(self):
+        return self.__passengers
+
+    def move(self, dist):
+        x = self.get_x() + dist * math.cos(self.get_angel())
+        self.set_x(x)
+        y = self.get_y() + dist * math.sin(self.get_angel())
+        self.set_y(y)
+
+        out_passenger = int(input('Кол-во вышедших пассажиров:'))
+        self.__passengers -= out_passenger
+        if self.__passengers < 0:
+            self.__passengers = 0
+
+        in_passenger = int(input('Кол-во вошедших пассажиров: '))
+        free_places = self.__max_passengers - self.get_passengers()
+        if free_places < in_passenger:
+            print(f'Места кончаются, можем взять только {free_places}')
+            self.__money += free_places * self.__ticket
+            self.__passengers += free_places
+        else:
+            self.__money += in_passenger * self.__ticket
+            self.__passengers += in_passenger
+
+# поворот влево - 0, вправо - 1. не стал заморачиваться, сделал поворот на 90 градусов за раз.
