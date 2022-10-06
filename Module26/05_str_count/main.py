744fd2b74b83f44af70aeba41bbe2bd46f4b5be5
chingizmalyukov <killerban4@gmail.com>
2022-10-06 16:45:32 +0300

    1, 2, 3, 5 tasks done

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,41 @@
-# TODO здесь писать код
+import os
+
+
+def string_count(file_path):
+    summ_strings = 0
+    empty_symbols = ['', ' ', '\n', '', '\t']
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for i_string in file:
+            string = 0
+            for i_symbol in i_string:
+                if i_symbol not in empty_symbols:
+                    string = 1
+                    break
+            summ_strings += string
+        print(f'Кол-во строк кода в файле {file_path} = {summ_strings}')
+        return summ_strings
+
+
+def file_check(start_path):
+    for i_item in os.listdir(start_path):
+        file_path = os.path.join(start_path, i_item)
+        # Хотел сделать дополнительно к заданию, чтобы функция проверяла файлы не только в указанной директории,
+        # но и в директориях внутри. Попытался использовать знания о рекурсии,
+        # но почему-то второй раз генератор внутри генератора не запускается
+        # if os.path.isdir(file_path):
+        #     file_check(file_path)
+        # else:
+        #     if file_path[len(file_path) - 3:] == '.py':
+        #         yield file_path
+        if file_path[len(file_path) - 3:] == '.py':
+            yield file_path
+
+
+start_path = input('Укажите директорию: ')
+
+solution = 0
+
+for file_path in file_check(start_path):
+    solution += string_count(file_path)
+
+print(f'Общее кол-во строк кода = {solution}')
