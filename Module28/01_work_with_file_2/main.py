d230650a110048d36a59fbff1cc5b65f05fa7fe2
chingizmalyukov <killerban4@gmail.com>
2022-10-24 18:09:26 +0300

    1 and 2 tasks done

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import os
+
+
+class File:
+    """Контекст-менеджер:
+        1)при попытке открыть несуществующий файл,
+     автоматически создаёт и открывает этот файл в режиме записи
+        2)на выходе из менеджера подавляются все исключения, связанные с файлами."""
+
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def __enter__(self):
+        if os.path.exists(self.file_name):
+            self.file = open(self.file_name, 'r', encoding='utf-8')
+        else:
+            self.file = open(self.file_name, 'a', encoding='utf-8')
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.file.close()
+        return True
+
+
+with File(file_name='test.txt') as test:
+    for i_string in test:  # для проверки подавления исключений
+        print(i_string)
--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+import math
+
+
+class MyMath:
+    """Класс с формулами из математики"""
+    import math
+
+    @classmethod
+    def circle_len(cls, radius: int) -> float:
+        return 2 * math.pi * radius
+
+    @classmethod
+    def circle_sq(cls, radius: int) -> float:
+        return math.pi * radius ** 2
+
+    @classmethod
+    def circle_vol(cls, radius: int, h: int) -> float:
+        return math.pi * radius ** 2 * h
+
+    @classmethod
+    def sphere(cls, radius: int) -> float:
+        return math.pi * 4 * radius ** 2
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+print(res_1)
+print(res_2)
