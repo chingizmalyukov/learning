9f8f6dd05bdfbfc767804f9799e65fd1742f9a3f
chingizmalyukov <killerban4@gmail.com>
2022-10-24 20:47:22 +0300

    all tasks done

--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -1,1 +1,75 @@
-# TODO здесь писать код
+from typing import Any
+from abc import ABC
+import math
+
+
+class Figure(ABC):
+    """Абстрактный класс фигура"""
+
+    def __init__(self, base: int, height: int) -> None:
+        self.base = base
+        self.height = height
+
+    @property
+    def base(self) -> int:
+        return self.__base
+
+    @base.setter
+    def base(self, base: int) -> None:
+        self.__base = base
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @height.setter
+    def height(self, height: int):
+        self.__height = height
+
+
+class Square(Figure):
+
+    def __init__(self, base: int) -> None:
+        super().__init__(base=base, height=base)
+
+    def square(self) -> int:
+        return self.base * 2
+
+    def perimetr(self) -> int:
+        return self.base * 4
+
+
+class Triangle(Figure):
+
+    def square(self) -> float:
+        return 1 / 2 * self.base * self.height
+
+    def perimetr(self) -> float:
+        return 2 * math.sqrt(self.base ** 2 - self.height ** 2) + 2 * self.base
+
+
+class FullSquareMixin:
+    figure_list = []
+    full_square = 0
+
+    def calculate(self) -> Any:
+        for item in list(self.figure_list):
+            self.full_square += item.square()
+        return self.full_square
+
+
+class Cube(FullSquareMixin):
+    def __init__(self, base: int):
+        self.figure_list = [Square(base) for _ in range(6)]
+
+
+class Pyramid(FullSquareMixin):
+    def __init__(self, base: int, height: int):
+        self.figure_list = [Triangle(base=base, height=height) for _ in range(4)]
+        self.figure_list.append(Square(base=base))
+
+
+cube_1 = Cube(4)
+print(cube_1.calculate())
+pyramid_1 = Pyramid(4, 6)
+print(pyramid_1.calculate())
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+class Date:
+    def __init__(self, day: int = 0, mouth: int = 0, year: int = 0) -> None:
+        self.day = day
+        self.mouth = mouth
+        self.year = year
+
+    def __str__(self) -> str:
+        return 'День: {}\tМесяц {}\tГод {}'.format(
+            self.day, self.mouth, self.year
+        )
+
+    @classmethod
+    def is_date_valid(cls, date: str) -> bool:
+        day, mouth, year = map(int, date.split('-'))
+        return 0 < day <= 31 and 0 < mouth <= 12 and 0 < year <= 9999
+
+    @classmethod
+    def from_string(cls, date: str) -> 'Date':
+        day, mouth, year = map(int, date.split('-'))
+        date_obj = cls(day, mouth, year)
+        return date_obj
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
