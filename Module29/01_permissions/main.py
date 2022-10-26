c2338ac33cc780f4185d666820b2c8151abac700
chingizmalyukov <killerban4@gmail.com>
2022-10-26 18:42:32 +0300

    1 and 2 tasks done

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def check_permission(user: str) -> Callable:
+    def check_access(funct: Callable) -> Callable:
+        def wrapped_funct(*args, **kwargs) -> None:
+            if user_permissions[0] == user:
+                funct(*args, **kwargs)
+            else:
+                print(f'PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию {funct.__name__}')
+            return
+
+        return wrapped_funct
+
+    return check_access
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+app = {}
+
+
+def callback(route):
+    def wrapper(func):
+        app[route] = func
+
+        def wrapped():
+            ret = func()
+            return ret
+
+        return wrapped
+
+    return wrapper
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+# я списал это задание, но я НЕ понимаю как это работает! функция example не вызывается нигде в коде.
