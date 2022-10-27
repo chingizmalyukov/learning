60ceb6d6ede5a4b1b2d6b01e4521e74e53d191a4
chingizmalyukov <killerban4@gmail.com>
2022-10-27 20:51:22 +0300

    all dasks done, thanks god!

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def decorator_with_args_for_any_decorator(decorator_to_enhance):
+    def decorator_maker(*args, **kwargs):
+        def decorator_wrapper(func):
+            return decorator_to_enhance(func, *args, **kwargs)
+
+        return decorator_wrapper
+
+    return decorator_maker
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func, *args, **kwargs):
+    def wrapper(text, num):
+        print("Переданные арги и кварги в декоратор:", args, kwargs)
+        return func(text, num)
+
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет,", text, num)
+
+
+decorated_function("Юзер", 101)
+#эту задачу я тоже не смог сразу решить. пришлось читать\смотреть разборы,
+#у меня вопрос: это задача сложная, или за 5, примерно 8 минутных ролика,
+#нереально объяснить эту тему?
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def singleton(cls):
+    def wrapper_singleton(*args, **kwargs):
+        if not wrapper_singleton.instance:
+            wrapper_singleton.instance = cls(*args, **kwargs)
+        return wrapper_singleton.instance
+
+    wrapper_singleton.instance = None
+    return wrapper_singleton
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
+
+#я даже пытаться не стал. со мной может кто-то позаниматься по этой теме, а то я ничего не понял,
+#или мне и дальше гуглить?
