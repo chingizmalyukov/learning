d56c551bcce767fc85520586d3bab9fbe7e10855
Alexandra <aguseva@mail.com>
2022-11-11 16:11:24 +0100

    решения зачтены

--- Module31/01_lorem_ipsum/main.py
+++ Module31/01_lorem_ipsum/main.py
@@ -6,7 +6,12 @@ nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretiu
 Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate 
 """
 patern = r'\b\w{4}\b'
-solution = re.findall(patern, text)
+# solution = re.findall(patern, text)
+solution = re.findall(r'\b\w{4}\b', text)
 print(solution)
-# если я не присваиваю патерн переменной, а сразу его вбиваю в функцию, то она ничего не находит
+# если я не присваиваю паттерн переменной, а сразу его вбиваю в функцию, то она ничего не находит
 # solution = re.findall(r'\\b\\w{4}\\b', text), почему так?
+# NOTE потому, что экранировать символы слэшей не нужно
+#  Обратите внимание на код в 10 строке - он работает так же, как и Ваш исходный на 9 строке
+#  Символ r вначале строке так же означает, что строка "сырая" (row string)
+#  то есть все специальные символы в ней экранированы по-умолчанию
--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -8,4 +8,9 @@ patern_2 = r'[<].*?[>]'
 cutter = re.findall(patern, data.text)
 solution = re.sub(patern_2, '', str(cutter))
 
-print(solution)
+print(solution)  # так тоже можно
+
+# NOTE обратите внимание и на такой способ решения этой задачи
+req_site = requests.get('http://www.columbia.edu/~fdc/sample.html')
+result = re.findall(r'<h3.*>(.*)</h3>', req_site.text)
+print(result)
